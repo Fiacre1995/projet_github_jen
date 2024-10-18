fafa1b19d0493a97c41d35631b780d75c6af379b
Fiacre1995 <a.fiacre.dion@gmail.com>
2024-10-18 23:00:09 +0200

    ajout du fichier src

---
+++ src/main/java/projet_github/script.java
@@ -0,0 +1,28 @@
+package projet_github;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class script {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.navigate().to("https://www.google.com/");
+		
+		driver.manage().window().fullscreen();
+		// Utiliser une variable
+		WebElement variable_search = driver.findElement(By.name("q"));
+		variable_search.sendKeys("voiture 2024");
+		//Permet d'imiter la touche "Entrer" après la saisie
+		variable_search.submit();
+
+	}
+}
